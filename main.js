39439c189c79e49e180b54d08b858532f089d222
Kenton Mail <kentonanders@gmail.com>
2022-04-05 16:29:01 -0400

    Flash messages ajouté

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-/********************
- *      Imports     *
- ********************/
 const express = require("express");
 const app = express();
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
+const connectFlash = require('connect-flash');
 dotenv.config({ path: "./configuration.env" });
 
 const Product = require('./models/product');
@@ -14,40 +14,39 @@ const methodOverride = require('method-override');
 const controller = require("./controllers/homeController");
 const routes = require('./routes/routes');
 
-/*********************
- *  Base de Données  *
- *********************/
+
 const DATABASE_LOCAL = process.env.DATABASE_LOCAL;
 mongoose.connect(DATABASE_LOCAL, {
   useNewUrlParser: true,
 });
 
-app.use(methodOverride('_method')); 
-/***********************
- * Fichiers Statiques  *
- ***********************/
-// app.use(express.static(path.resolve(__dirname, "/public")));
-app.use(express.static("public"));
-
-/*********************
- *  Views/Middleware *
- *********************/
-app.use(expressLayouts);
 app.set("layout", "./layouts/myLayouts");
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
-app.set("views", path.resolve(__dirname + "/views"));
 app.set("view engine", "ejs");
 
-/*********************
- *       Routes      *
- *********************/
+app.use(methodOverride('_method')); 
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+app.use(cookieParser('secretStringForCookies'));
+app.use(expressSession({
+  secret: 'secretStringForCookies',
+  cookie: {
+    maxAge: 4000000
+  },
+  resave: false,
+  saveUnitialized: false,
+}));
+app.use(connectFlash());
+
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
 app.use(routes);
 
-
-/*********************
- *         Port      *
- *********************/
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
